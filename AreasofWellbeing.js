2383560b0bae08a30e1d97034796a7abe45c4a97
Ieuan Payne <ieuanpayne@MacBook-Pro.local>
2021-03-27 01:12:04 +0000

    adding logic in prep for JS

---
+++ AreasofWellbeing.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('try-quiz')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    console.log('Started')
+}
+
+function setNextStatement(){
+
+}
+
+function selectStatement(){
+
+}
